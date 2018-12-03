19a8e9f7643c186d369ece4804a526f942a5bfaa
lechinh <>
2018-12-03 22:09:02 +0700

    no message

---
+++ Quản Lý Hàng Hoá/Quản Lý Hàng Hoá/List Goods/ListGoods.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quản Lý Hàng Hoá
+//
+//  Created by Admin on 12/3/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ListGoods: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
