2e3d46c8bbf888ffe715277f1e6880c9b7244e00
lechinh <>
2018-12-04 09:06:54 +0700

    no message

--- Quản Lý Hàng Hoá/Quản Lý Hàng Hoá/Add New Goods/AddNewGoods.swift
+++ Quản Lý Hàng Hoá/Quản Lý Hàng Hoá/Add New Goods/AddNewGoods.swift
@@ -9,26 +9,10 @@
 import UIKit
 
 class AddNewGoods: UIViewController {
-
-    @IBOutlet weak var btnAdd: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(patternImage: UIImage(named: "bg")!)
-        btnAdd.tintColor = UIColor.red
-        btnAdd.setBackgroundImage(UIImage(named: "linear"), for: UIControl.State.normal, barMetrics: UIBarMetrics.default)
-        // testttttt
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
